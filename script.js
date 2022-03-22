a42a40a0c577352378ac9d36f543eb2e884e490a
Yana_Saitova <syanaa9@gmail.com>
2022-03-22 21:35:53 +0900

    feat: continue develop of popup dialog -js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Initialize Variables
 let closePopup = document.querySelector(".popup_close");
 let popup = document.querySelector(".popup");
-//let profileEditPopup = document.querySelector(".popup__container");
-let button = document.querySelector(".profile__edit-button");
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__about");
+let formName = document.querySelector(".input__text_type_name");
+let formAbout = document.querySelector(".input__text_type_about");
+
+let saveForm = document.querySelector(".popup__save-button");
+let openForm = document.querySelector(".profile__edit-button");
+
+// Save Popup Event
+
+
+saveForm.onclick = function () {
+  profileName.textContent = formName.value;
+  profileAbout.textContent = formAbout.value;
+  popup.classList.remove('popup_opened');
+};
+
 // Close Popup Event
 closePopup.onclick = function () {
   //popup.style.display = 'none';
@@ -11,10 +26,41 @@ closePopup.onclick = function () {
 
 };
 // Show popup and Popup
-button.onclick = function () {
+openForm.onclick = function () {
   //popup.style.display = 'block';
+  formName.value = profileName.textContent;
+  formAbout.value = profileAbout.textContent;
   popup.classList.add('popup_opened');
   //profileEditPopup.style.display = 'block';
+
 }
 
+//addButton.addEventListener('click', addSong);
 
+
+
+
+
+//// Находим форму в DOM
+//let formElement = document.querySelector('.popup')
+//// Находим поля формы в DOM
+//let nameInput = // Воспользуйтесь инструментом .querySelector()
+//  let jobInput = // Воспользуйтесь инструментом .querySelector()
+
+//    // Обработчик «отправки» формы, хотя пока
+//    // она никуда отправляться не будет
+//    function formSubmitHandler(evt) {
+//      evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+//      // Так мы можем определить свою логику отправки.
+//      // О том, как это делать, расскажем позже.
+
+//      // Получите значение полей jobInput и nameInput из свойства value
+
+//      // Выберите элементы, куда должны быть вставлены значения полей
+
+//      // Вставьте новые значения с помощью textContent
+//    }
+
+//// Прикрепляем обработчик к форме:
+//// он будет следить за событием “submit” - «отправка»
+//formElement.addEventListener('submit', formSubmitHandler);
