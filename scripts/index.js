a4a27d78a5ca7ccfe284252fa1084c36e6700d1d
Yana_Saitova <syanaa9@gmail.com>
2022-04-08 21:43:44 +0900

    add initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -34,3 +34,44 @@ openForm.addEventListener('click', openPopup);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+function createCard(name, link) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const newCard = cardTemplate.cloneNode(true);
+  const image = newCard.querySelector('.card__picture');
+  const title = newCard.querySelector('.card__title');
+  image.src = link;
+  image.alt = name;
+  title.textContent = name;
+  document.querySelector('.gallery').appendChild(newCard);
+};
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(card => createCard(card.name, card.link));
+
